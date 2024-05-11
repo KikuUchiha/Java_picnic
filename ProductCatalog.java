e057c01c9637121172d4ab6b2143a9e3103403fa
Kiku Uchiha <n.u.10.10@mail.ru>
2024-05-11 05:13:45 +0300

    Добавила конструктор

--- ProductCatalog.java
+++ ProductCatalog.java
@@ -1,3 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductCatalog {
+    private Map<String, Boolean> fruits;
+    private Map<String, Boolean> vegetables;
+
+    public ProductCatalog() {
+        fruits = new HashMap<>();
+        vegetables = new HashMap<>();
+        initializeFruits();
+        initializeVegetables();
+    }
+
+    private void initializeFruits() {
+    }
+
+    private void initializeVegetables() {
+    }
 
 }
