7d09d13b2094525019b278bcfb17378de7d76bc7
Kiku Uchiha <n.u.10.10@mail.ru>
2024-05-11 05:08:55 +0300

    Добавила методы: нахождение самого длинного слова и частота слова

--- WordCounter.java
+++ WordCounter.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordCounter {
@@ -18,6 +20,31 @@ public class WordCounter {
         }
         scanner.close();
         return count;
+    }
+
+    //находит самое длинное слово в файле
+    public String findLongestWord() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String longestWord = "";
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        scanner.close();
+        return longestWord;
+    }
+    //частота каждого слова в файле
+    public Map<String, Integer> wordFrequency() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+        }
+        scanner.close();
+        return frequencyMap;
     }
 
 }
