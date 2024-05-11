76f37a59d5c0c483b2e4ce0e73cc0d1c0956f3e1
Kiku Uchiha <n.u.10.10@mail.ru>
2024-05-11 05:07:36 +0300

    Добавила метод подсчёта слов в файле

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordCounter {
+    private File file;
+
+    public WordCounter(File file) {
+        this.file = file;
+    }
+    //считает количество слов в файле
+    public int countWords() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        int count = 0;
+        while (scanner.hasNext()) {
+            scanner.next();
+            count++;
+        }
+        scanner.close();
+        return count;
+    }
 
 }
